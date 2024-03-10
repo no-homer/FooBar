660fd7965e4667fa68be7b38e36abb14be91b4af
Joern Guderian <joern.guderian@gmx.de>
2024-03-10 15:44:17 +0100

    Initialer commit.

--- src/com/toast/brot/HalloWelt.java
+++ src/com/toast/brot/HalloWelt.java
@@ -3,7 +3,7 @@ package com.toast.brot;
 public class HalloWelt {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello lovely World");
 	}
 
 }
